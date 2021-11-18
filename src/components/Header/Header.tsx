13aa23f1cd6530677d8654dbe6a75b72d447d9d4
Mikhail Shestak <stampout386@gmail.com>
2021-11-18 14:28:39 +0300

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div>
+            <a href="#">-Home-</a>
+            <a href="#">-New feeds-</a>
+            <a href="#">-Messages-</a>
+
+        </div>
+    )
+}
+
+export default Header;
