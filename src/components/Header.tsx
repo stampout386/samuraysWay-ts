7afa4860399f231b5a044547940f7b8a0b895ea8
Mikhail Shestak <stampout386@gmail.com>
2021-12-09 15:37:40 +0300

    12 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Header() {
+    return (
+        <header className='header'>
+            <img src="https://i.pinimg.com/originals/09/17/04/0917045fcee27f48e768680d1f800577.jpg" alt=""/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Profile(){
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg"
+                    alt=""/>
+            </div>
+            <div>ava+description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
