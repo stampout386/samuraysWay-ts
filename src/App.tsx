c2ce6772545a6563c45d5bcb904c2b20ddb0cebc
Mikhail Shestak <stampout386@gmail.com>
2021-12-09 15:13:42 +0300

    11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
+
 
 function App() {
     return (
-        <div>
-            <Header/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://i.pinimg.com/originals/09/17/04/0917045fcee27f48e768680d1f800577.jpg" alt=""/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messeges</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg"
+                        alt=""/>
+                </div>
+                <div>ava+description</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
